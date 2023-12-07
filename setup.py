d54fab2e5f34356af9ac642bd74497d22f8278b6
Srinubabuvelugu <srinubabu1911@gmail.com>
2023-12-07 10:12:18 +0530

    steup added

--- setup.py
+++ setup.py
@@ -1,22 +1,33 @@
-import os
 import setuptools
 
 __version__ = '0.0.1'
-repo_name = "CNN-Project-structure"
-author_git_url = "https://github.com/Srinubabuvelugu"
 
-setuptools.,setup(
-    name = "CNN Project Structure",
-    version = '0.0.1',
-    author = "Velugu Srinu Babu",
-    author_email = "srinubabu1119@gmail.com",
-    description = "This is the Deep Learning Classification Project",
-    long_description = long_description,
-    long_description_content = "text/markdown",
-    url= f"{author_git_url}/{repo_name}",
-    project_urls = {
-        "Bug Tracker": f"{author_git_url}/{repo_name}/issues",
-    },
-    package_dir = {"":"src"},
-    packages = setuptools.find_packages(where="src")
-)
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description=f.read()
+
+REPO_NAME="CNN-Project-Structure"
+AUTHOR_NAME="Srinubabu velugu"
+SRC_REPO="CNNclassifier"
+AUTHOR_Email="srinubabu1119@gmail.com"
+
+setuptools.setup(
+        name=SRC_REPO,
+        version=__version__,
+        author=AUTHOR_NAME,
+        author_email=AUTHOR_Email,
+        description="this is my classifcation project",
+        long_description=long_description,
+        long_description_content="text/markdown",
+        url=f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}",
+        
+        project_urls={
+                      
+        "Bug Tracker": f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}/issues"  
+                      
+                    },
+        package_dir={"":"src"},
+        packages=setuptools.find_packages(where="src")
+                 
+                 
+                 
+                )
